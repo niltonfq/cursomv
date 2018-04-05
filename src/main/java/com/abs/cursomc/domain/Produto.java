5a4b7498ca355257422a00238394b06fdf04aa04
niltonfq <niltonfq@gmail.com>
2018-04-05 16:26:58 -0300

    excecao endpoint

--- src/main/java/com/abs/cursomc/domain/Produto.java
+++ src/main/java/com/abs/cursomc/domain/Produto.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import org.hibernate.annotations.ManyToAny;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Produto implements Serializable{
@@ -25,6 +25,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name="PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name="produto_id"),
