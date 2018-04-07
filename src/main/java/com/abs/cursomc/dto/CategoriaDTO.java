da8c7ad08d0a190bc2efd73dcd109ca9d9365d82
niltonfq <niltonfq@gmail.com>
2018-04-06 23:37:45 -0300

    validacao dto conversao dto domain

--- src/main/java/com/abs/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/abs/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.abs.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.abs.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -9,6 +13,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="Tamanho entre 5 e 80")
 	private String nome;
 	
 	public CategoriaDTO() {
