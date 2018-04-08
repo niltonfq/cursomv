9f6afd763d46b12cad57255a1cf9741df9170015
niltonfq <niltonfq@gmail.com>
2018-04-08 12:13:30 -0300

    consulta com @query

--- src/main/java/com/abs/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/abs/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,26 @@
 package com.abs.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.abs.cursomc.domain.Categoria;
 import com.abs.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+	@Transactional(readOnly=true)
+	@Query("Select distinct obj from Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+	Page<Produto> search(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 
 }
---
+++ src/main/java/com/abs/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.abs.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.abs.cursomc.domain.Cliente;
+import com.abs.cursomc.dto.ClienteDTO;
+import com.abs.cursomc.repositories.ClienteRepository;
+import com.abs.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings({ "unchecked" })
+		Map<String, String> map = (Map<String, String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente cli = repo.findByEmail(objDto.getEmail());
+		if (cli != null && !cli.getId().equals(uriId)) {
+			list.add(new FieldMessage("Email", "e-mail já cadastrado"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
