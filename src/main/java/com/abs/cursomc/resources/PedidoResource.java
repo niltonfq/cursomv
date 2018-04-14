36ef401955efb43224c35afc88a70eac5fdc07fa
niltonfq <niltonfq@gmail.com>
2018-04-14 13:37:36 -0300

    refresh token

--- src/main/java/com/abs/cursomc/resources/PedidoResource.java
+++ src/main/java/com/abs/cursomc/resources/PedidoResource.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.abs.cursomc.domain.Categoria;
 import com.abs.cursomc.domain.Pedido;
-import com.abs.cursomc.dto.CategoriaDTO;
 import com.abs.cursomc.services.PedidoService;
 
 @RestController
